8d870c1fde29869fbb50b427c28bc0139c4a3fa9
jinzhi <admin@end.wiki>
2017-10-22 21:58:37 +0800

    增加 Restful 控制器

--- app/api/controller/Restful.php
+++ app/api/controller/Restful.php
@@ -1,7 +1,30 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Restful 控制器(父类)
  * User: end_wiki
  * Date: 2017/10/22
  * Time: 21:15
  */
+namespace app\api\controller;
+
+use src\framework\Request;
+use src\framework\Route;
+use app\common\exceptions\HttpRequestMethodException;
+
+class Restful {
+
+    private $actionMethodMap = [
+        'add'       =>              'put',
+        'update'    =>              'post',
+        'delete'    =>              'delete',
+        'get'       =>              'get',
+    ];
+
+    public function __construct(){
+        // 检查 HTTP 方法
+        if(strtolower(Request::getMethod()) !==
+            strtolower($this->actionMethodMap[Route::getAction()])){
+            throw new HttpRequestMethodException();
+        }
+    }
+}
