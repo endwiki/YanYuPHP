7f235a568b158378a16cbf1f00cdff295a09a1cc
jinzhi <admin@end.wiki>
2017-11-19 16:32:46 +0800

    新增文本翻译接口

--- app/common/exceptions/TextTranslateFailedException.php
+++ app/common/exceptions/TextTranslateFailedException.php
@@ -1,7 +1,13 @@
 <?php
 /**
- * Created by PhpStorm.
+ * 翻译失败异常类
  * User: end_wiki
  * Date: 2017/11/19
  * Time: 15:54
  */
+namespace app\common\exceptions;
+
+class TextTranslateFailedException extends ExceptionHandler {
+    protected $code = 600002;
+    protected $message = '翻译失败，请稍后再试!';
+}
