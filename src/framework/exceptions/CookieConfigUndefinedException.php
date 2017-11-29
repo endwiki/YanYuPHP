d2513f05202758231eeb5757b04aabe47cabd2f8
junxiao <admin@end.wiki>
2017-11-29 09:54:24 +0800

    增加对 Cookie 的支持

--- src/framework/exceptions/CookieConfigUndefinedException.php
+++ src/framework/exceptions/CookieConfigUndefinedException.php
@@ -1,7 +1,13 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: xiao
+ * Cookie 配置未定义异常
+ * User: end_wiki
  * Date: 2017/11/27
  * Time: 14:15
  */
+namespace src\framework\exceptions;
+
+class CookieConfigUndefinedException extends ExceptionHandler{
+    protected $code = 100031;
+    protected $message = 'Cookie 配置未定义!';
+}
