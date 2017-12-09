a4f27fef26abc300bf56b90ae3841301b6664e83
junxiao <admin@end.wiki>
2017-12-09 18:00:35 +0800

    实现对Redis缓存的支持

--- src/framework/Cache.php
+++ src/framework/Cache.php
@@ -14,6 +14,7 @@ class Cache {
 
     protected static $namespace = 'src\\framework\\caches\\';        // 命名空间
     protected static $instance;                                      // 保存实例
+    protected static $i;
 
     /**
      * 获取缓存实例
@@ -21,7 +22,13 @@ class Cache {
      * @return self::$instance
      */
     public static function getInstance(){
-        $cacheType = (Config::get('CACHE.TYPE'));
+        self::$i++;
+        if(self::$i == 2){
+            debug_print_backtrace();
+            die();
+        }
+
+        $cacheType = Config::get('CACHE.TYPE');
         $className = self::$namespace . ucfirst(strtolower($cacheType));
         if(!isset(self::$instance)){
             try{
@@ -31,5 +38,27 @@ class Cache {
             }
         }
         return self::$instance;
+    }
+
+    /**
+     * 设置缓存
+     * @param String $key 键名
+     * @param String $value 值
+     * @return mixed
+     */
+    public static function set(String $key,String $value){
+        if(!isset(self::$instance)){
+            self::getInstance();
+        }
+        $result = self::$instance->set($key,$value,Config::get('CACHE.EXPIRATION'));
+        return $result;
+    }
+
+    public static function get(String $key){
+        if(!isset(self::$instance)){
+            self::getInstance();
+        }
+        $value = self::$instance->get($key);
+        return $value;
     }
 }
---
+++ src/framework/caches/Redis.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 缓存-Redis 实现类
+ * User: end_wiki
+ * Date: 2017/12/9
+ * Time: 11:44
+ */
+namespace src\framework\caches;
+
+use src\framework\Config;
+use src\framework\Database;
+
+class Redis {
+
+    protected $instance;
+
+
+    public function __construct(){
+        $this->instance = Database::getInstance(Config::get('CACHE.INSTANCE_NAME'));
+    }
+
+    /**
+     * 获取缓存
+     * @param String $key 键名
+     * @return mixed
+     */
+    public function get(String $key){
+        $result = $this->instance->get($key);
+        return $result;
+    }
+
+    /**
+     * 设置缓存
+     * @param String $key 键名
+     * @param String $value 值
+     * @param int $timeout 生存时间(秒)
+     * @return mixed
+     */
+    public function set(String $key,String $value,int $timeout){
+        $result = $this->instance->set($key,$value,$timeout);
+        return $result;
+    }
+}
